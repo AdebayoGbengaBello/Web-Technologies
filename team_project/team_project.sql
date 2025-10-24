456e3131f0ec074b1b10d8646e026c765540d0f8
AdebayoGbengaBello <adebayo.bello@ashesi.edu.gh>
2025-10-24 14:22:03 +0000

    Added Products Table Login Page

--- team_project/team_project.sql
+++ team_project/team_project.sql
@@ -7,3 +7,14 @@ CREATE Table Developers(
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (developer_id)
 );
+
+CREATE TABLE Products(
+    product_id INT(11) NOT NULL AUTO_INCREMENT,
+    developer_id INT(11) NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    linked_url VARCHAR(255),
+    PRIMARY KEY (product_id),
+    FOREIGN KEY (developer_id) REFERENCES Developers(developer_id)
+);
